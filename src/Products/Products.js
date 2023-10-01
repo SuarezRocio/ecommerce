b5e3cf27ec4ae3ed407cf270ed7bac0996dd3a97
SuarezRocio <suarez.rocio4344@gmail.com>
2023-10-01 02:27:08 -0300

    subiendo primeros cambios

---
+++ src/Products/Products.js
@@ -0,0 +1,136 @@
+import {BsFillBagHeartFill} from "react-icons/bs"
+import { AiFillStar } from "react-icons/ai"
+import "./Products.css"
+
+
+function Products  ()  {
+  return (
+    <>
+    <section className="card-container">
+      <section 
+      className="card">
+      <img src="https://i.ibb.co/NZYtkWw/capa-tecido.png" 
+      alt="capa de tablet"
+      className="card-img"
+      />
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+      
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+      
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+      
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+      
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+      
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+      
+      <div className="card-details">
+          <h3 className="card-title"> 
+            Tecnologia
+          </h3>
+          <section className="card-rewies">
+              <AiFillStar className="ratings-start"/> <AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/><AiFillStar className="ratings-start"/>
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div>$300</div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+          </section>
+      </div>
+    </section>
+    </section>
+  
+  
+    </>
+  )
+}
+
+
+export default Products
---
+++ src/Recomended/Recomended.js
@@ -0,0 +1,32 @@
+import "./Recomended.css"
+
+
+function Recomended ()  {
+  return (
+    <>
+      <div className="recommended-flex">
+        <h2>Recomended</h2>
+        <div className="recommended-btns">
+          <button className="btns">
+            All Products
+          </button>
+          <button className="btns">
+          Sonic
+          </button>
+          <button className="btns">
+          Amazon
+          </button>
+          <button className="btns">
+          GloboPlay
+          </button>
+          <button className="btns">
+          Eco Show
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+export default Recomended
